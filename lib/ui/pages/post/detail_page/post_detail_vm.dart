9b5e99c5543594cb14fb8bb66d6b3c1e2b02d320
GUNWO0 <kk000624@kyungsung.ac.kr>
2025-06-09 17:19:36 +0900

    ãㅇã게시글 상세보기

---
+++ lib/ui/pages/post/detail_page/post_detail_vm.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog/data/model/post.dart';
+import 'package:flutter_blog/data/repository/post_repository.dart';
+import 'package:flutter_blog/main.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// 1. 창고 관리자
+final postDetailProvider = NotifierProvider.family<PostDetailVM, PostDetailModel?, int>(() {
+  return PostDetailVM();
+});
+
+// TODO 3 : init 완성하기 (state 갱신)
+// 2. 창고 (상태가 변경 되어도, 화면 갱신 안함 - watch 하지마)
+class PostDetailVM extends FamilyNotifier<PostDetailModel?, int> {
+  final mContext = navigatorKey.currentContext!;
+
+  @override
+  PostDetailModel? build(int postId) {
+    init(postId);
+    return null;
+  }
+
+  Future<void> init(int postId) async {
+    Map<String, dynamic> body = await PostRepository().getOne(postId);
+    if (!body["success"]) {
+      ScaffoldMessenger.of(mContext!).showSnackBar(
+        SnackBar(content: Text("게시글 상세보기 실패 : ${body["errorMessage"]}")),
+      );
+      return;
+    }
+
+    state = PostDetailModel.fromMap(body["response"]);
+  }
+}
+
+// TODO 2 : replies 빼고 상태로 관리하기
+class PostDetailModel {
+  Post post;
+
+  PostDetailModel(this.post);
+
+  PostDetailModel.fromMap(Map<String, dynamic> data) : post = Post.fromMap(data);
+
+  PostDetailModel copyWith({
+    Post? post,
+  }) {
+    return PostDetailModel(post ?? this.post);
+  }
+
+  @override
+  String toString() {
+    return 'PostDetailModel(post: $post)';
+  }
+}

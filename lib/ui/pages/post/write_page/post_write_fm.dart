8546ae663a3e8e09b9856b7d2eb3cbacf45b914b
GUNWO0 <kk000624@kyungsung.ac.kr>
2025-06-10 10:54:14 +0900

    게시글 쓰기

---
+++ lib/ui/pages/post/write_page/post_write_fm.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// 1. 창고 관리자
+final PostWriteProvider = NotifierProvider<PostWriteFm, PostWriteModel>(() {
+  return PostWriteFm();
+});
+
+/// 2. 창고
+class PostWriteFm extends Notifier<PostWriteModel> {
+  @override
+  PostWriteModel build() {
+    return PostWriteModel("", "");
+  }
+
+  void title(String title) {
+    state = state.copyWith(
+      title: title,
+    );
+  }
+
+  void content(String content) {
+    state = state.copyWith(
+      content: content,
+    );
+  }
+}
+
+/// 3. 창고 데이터 타입
+class PostWriteModel {
+  final String title;
+  final String content;
+
+  PostWriteModel(
+    this.title,
+    this.content,
+  );
+
+  PostWriteModel copyWith({
+    String? title,
+    String? content,
+  }) {
+    return PostWriteModel(title ?? this.title, content ?? this.content);
+  }
+
+  @override
+  String toString() {
+    return 'PostWriteModel{title: $title, content: $content}';
+  }
+}
